187952c4e162912343aac4374ad33f3802c7c29f
Alex Stanciu <ealexs@gmail.com>
2024-03-08 08:53:13 +0200

    update

---
+++ public_html/php/index.php
@@ -0,0 +1,12 @@
+<?php
+
+Q_Descriptive_Server_Side::run(APP_CODE_DIR);
+
+final class Q_Descriptive_Server_Side
+{
+	public static function run(string $dir)
+	{
+		echo "loadded !!! @@" . $dir;
+	}
+}
+
